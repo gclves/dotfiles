9802c01bd7ab967eb4f55c4ecbd02d3ba4e85940
Guilherme Gonçalves <gsg@ggoncalves.me>
2017-02-23 18:48:05 -0300

    Remove Emacs stuff I shouldn't have commited

--- .emacs.d/elpa/queue-0.1.1/queue-autoloads.el
+++
@@ -1,25 +0,0 @@
-;;; queue-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil "queue" "queue.el" (21876 14948 151345 827000))
-;;; Generated autoloads from queue.el
-
-(defalias 'make-queue 'queue-create "\
-Create an empty queue data structure.")
-
-;;;***
-
-;;;### (autoloads nil nil ("queue-pkg.el") (21876 14948 277841 964000))
-
-;;;***
-
-(provide 'queue-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; queue-autoloads.el ends here
