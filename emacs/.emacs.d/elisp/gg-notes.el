3351b4af0a4b4ffba272fb1aa07f5e58b1f256c0
Gui Goncalves <gui.goncalves@fundingcircle.com>
2021-11-22 12:38:44 +0000

    Tweak some settings that really should be per-environment

--- emacs/.emacs.d/elisp/gg-notes.el
+++ emacs/.emacs.d/elisp/gg-notes.el
@@ -6,7 +6,7 @@
   (setq deft-extensions '("org" "md" "txt")
         deft-recursive t
         deft-default-extension "org"
-        deft-directory "~/sync/Notes"
+        deft-directory "~/Notes"
         deft-new-file-format "%Y%m%d%H%M"))
 
 (use-package org-roam
@@ -14,7 +14,7 @@
   (:map org-mode-map
         ("C-c i" . org-roam-insert))
   :config
-  (setq org-roam-directory "~/sync/Notes"
+  (setq org-roam-directory "~/Notes"
         org-roam-db-update-method 'immediate)
   (add-hook 'after-init-hook 'org-roam-mode))
 
@@ -44,19 +44,25 @@
 
 (add-hook 'text-mode-hook 'setup-text-mode)
 
+(defun setup-org ()
+  "Set up typography for Org-mode."
 ;;; Set up the typography
-(defvar gg--monospace-font "Go Mono-18"
-  "The font used for Monospace text within prose.")
-(defvar gg--body-font "Go-18"
-  "The font used for body text within prose.")
+  (defvar gg--monospace-font "Go Mono-18"
+    "The font used for Monospace text within prose.")
+  (defvar gg--body-font "Go-18"
+    "The font used for body text within prose.")
 
-(cl-loop for face in '(fixed-pitch org-code org-block)
-         do (set-face-attribute face nil :font gg--monospace-font))
+  (cl-loop for face in '(fixed-pitch org-code org-block)
+           do (set-face-attribute face nil :font gg--monospace-font))
 
-(set-face-attribute 'variable-pitch nil :font gg--body-font)
-(set-face-attribute 'org-quote nil :font gg--body-font :slant 'italic)
-;; org-table needs to be monospaced to be aligned
-(set-face-attribute 'org-table nil :font "Monaco-19")
+  (set-face-attribute 'variable-pitch nil :font gg--body-font)
+  (set-face-attribute 'org-quote nil :font gg--body-font :slant 'italic)
+  ;; org-table needs to be monospaced to be aligned
+  (set-face-attribute 'org-table nil :font "Monaco-19")
+  (set-face-attribute 'org-checkbox nil :font "Monaco-19"))
+
+;; XXX: Do we really need to run all that as a hook?!
+(add-hook 'org-mode-hook 'setup-org)
 
 ;; Org-powered presentations
 (require 'epresent)
