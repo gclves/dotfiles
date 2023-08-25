782b51896448301756de5c8a1c5314119a0809a0
Gui Goncalves <gui.goncalves@fundingcircle.com>
2023-08-25 22:13:02 +0100

    emacs: clever-er use of  `use-package'

--- emacs/.emacs.d/elisp/gg-use-package.el
+++ emacs/.emacs.d/elisp/gg-use-package.el
@@ -3,14 +3,21 @@
                          ("org" . "https://orgmode.org/elpa/")
                          ;; ("marmalade" . "https://marmalade-repo.org/packages/")
                          ("melpa" . "https://melpa.org/packages/")))
-(package-initialize)
 
 (unless package-archive-contents
   (package-refresh-contents))
 
 (if (package-installed-p 'use-package)
-    (setq use-package-always-ensure t)
-  (progn (package-initialize)
-         (package-install 'use-package)))
+    (progn
+      (setq use-package-always-ensure t)
+      (package-initialize))
+  ;; Note: use-package is installed by default on Emacs 29+
+  (if (not (yes-or-no-p (concat "Refresh packages, install use-package and"
+                                " other packages used by init file? ")))
+      (error "you need to install use-package first")
+    (package-refresh-contents)
+    (package-install 'use-package)
+    (require 'use-package)
+    (setq use-package-always-ensure t)))
 
 (provide 'gg-use-package)
