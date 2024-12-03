3d847bd0c2c5db859fbc6e0c345306236a3a846c
gg <gg@gclv.es>
2024-12-03 19:30:29 -0300

    emacs: adopt projectile
    project.el is clearly not working for me

--- emacs/.config/emacs/elisp/gg-project.el
+++ emacs/.config/emacs/elisp/gg-project.el
@@ -7,6 +7,20 @@
   (find-file gg-todo-file))
 (global-set-key (kbd "<f4>") 'gg-todo)
 
+(use-package rg)
+
+(use-package projectile
+  :ensure t
+  :init
+  (projectile-mode +1)
+  :config
+  (setq projectile-project-search-path '("~/src"))
+  :bind ((:map projectile-mode-map
+               ("s-p" . projectile-command-map)
+               ("C-x p" . projectile-command-map))
+         (:map global-map
+               ("s-t" . projectile-find-file))))
+
 (with-eval-after-load 'project
   (global-set-key (kbd "s-t") 'project-find-file))
 
