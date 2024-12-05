81100f80b7ef8d8fb56a3a586e1f7d78daa1ca22
gg <gg@gclv.es>
2024-12-05 13:32:01 -0300

    emacs: bump notmuch

--- emacs/.config/emacs/elisp/gg-mail.el
+++ emacs/.config/emacs/elisp/gg-mail.el
@@ -3,22 +3,25 @@
    notmuch-hello-sections
    '(notmuch-hello-insert-saved-searches notmuch-hello-insert-search notmuch-hello-insert-alltags notmuch-hello-insert-footer)
    notmuch-saved-searches
-        '((:name "inbox" :query "tag:inbox" :key
-                 [105])
-          (:name "newsletters" :query "tag:newsletters")
-          (:name "unread" :query "tag:unread" :key
-                 [117])
-          (:name "flagged" :query "tag:flagged" :key
-                 [102])
-          (:name "sent" :query "tag:sent" :key
-                 [116])
-          (:name "drafts" :query "tag:draft" :key
-                 [100])
-          (:name "all mail" :query "*" :key
-                 [97])))
+   '((:name "inbox" :query "tag:inbox and not tag:archived" :key "i")
+     (:name "newsletters" :query "tag:newsletters and not tag:archived")
+     (:name "unread" :query "tag:unread and not tag:archived" :key
+            [117])
+     (:name "flagged" :query "tag:flagged" :key
+            [102])
+     (:name "sent" :query "tag:sent" :key
+            [116])
+     (:name "drafts" :query "tag:draft" :key
+            [100])
+     (:name "all mail" :query "*" :key
+            [97]))
+   notmuch-archive-tags '("-inbox" "+archived")
+   notmuch-tree-outline-mode t)
+
+  (defalias 'gg-goto-mailbox 'notmuch-jump-search)
 
   (defun gg--notmuch-delete-message ()
-      "Mark a message to be deleted"
+    "Mark a message to be deleted"
     (interactive)
     (notmuch-search-tag '("+deleted" "-inbox"))
     (notmuch-search-next-thread))
