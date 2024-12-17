d993a78573adf6999879eba6b9d4103901c8607b
gclv <gg@gclv.es>
2024-12-17 10:48:00 -0300

    emacs: bump macosx config

--- emacs/.config/emacs/elisp/gg-osx-config.el
+++ emacs/.config/emacs/elisp/gg-osx-config.el
@@ -1,9 +1,8 @@
-(progn
-    (menu-bar-mode +1)
-    (add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
-    (add-to-list 'default-frame-alist '(ns-appearance . dark))
-    (setq ns-use-proxy-icon  nil)
-    (setq frame-title-format nil)
-    (setq locate-command "mdfind"))
+(menu-bar-mode +1)
+(add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
+(add-to-list 'default-frame-alist '(ns-appearance . dark))
+(setq ns-use-proxy-icon  nil)
+(setq frame-title-format nil)
+(setq locate-command "mdfind")
 
 (provide 'gg-osx-config)
