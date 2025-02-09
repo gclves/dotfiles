c90d769b16880499d6f5d38006a72362f12a2189
Gui Gonçalves <gui@liveflow.io>
2025-02-09 22:52:49 +0000

    emacs: bump my elisp script to run mix

--- emacs/.config/emacs/elisp/gg-elixir.el
+++ emacs/.config/emacs/elisp/gg-elixir.el
@@ -11,16 +11,24 @@
 
 (add-to-list 'auto-mode-alist '("\\.exs?\\'" . elixir-ts-mode))
 
+(defun gg--iex-buffer-for-project (project-root args)
+  "Return the iex buffer for the project `PROJECT-ROOT', creating one with `ARGS' if it does not exist."
+  ;; FIXME: this does not support multiple project yet (bc. *iex* is hardcoded)
+  (let ((buffer-name "*iex*"))
+    (if (comint-check-proc buffer-name)
+        buffer-name
+      (let ((default-directory project-root))
+        (apply 'make-comint-in-buffer "iex" nil "iex" nil args)))))
+
 (with-eval-after-load 'comint
   (defun gg-run-iex ()
     "Runs an IEx sesssion with `comint-mode' on the current project."
     (interactive)
-    (let ((buffer-name "*iex*")
-          (project-root (or (projectile-project-root) default-directory)))
-      (if (comint-check-proc buffer-name)
-          (pop-to-buffer buffer)
-        (let ((default-directory project-root))
-          (pop-to-buffer
-           (apply 'make-comint-in-buffer "iex" nil "iex" nil '("-S" "mix"))))))))
+    ;; FIXME: breaks if projectile is not loaded
+    (let* ((project-root (or (projectile-project-root) default-directory))
+           (args-str (read-string "Run iex with arguments: "))
+           (args (if (string= args-str "") nil (split-string args-str)))
+           (buffer (gg--iex-buffer-for-project project-root args)))
+      (pop-to-buffer buffer))))
 
 (provide 'gg-elixir)
