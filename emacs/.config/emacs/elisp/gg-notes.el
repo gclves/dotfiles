0e64e19c09e81b541b164760dc211a2ea1441ae9
Gui Goncalves <guilherme.goncalves@ibusinessfunding.com>
2026-01-09 14:19:05 -0300

    emacs: can't believe i'm using org-mode again :D

--- emacs/.config/emacs/elisp/gg-notes.el
+++ emacs/.config/emacs/elisp/gg-notes.el
@@ -1,7 +1,9 @@
 (require 'gg-scratch)
 
 (with-eval-after-load 'org
-  (global-set-key (kbd "C-x C-l") 'org-store-link))
+  (global-set-key (kbd "C-x C-l") 'org-store-link)
+  (setq org-log-done 'time
+        org-todo-keywords '((sequence "TODO(t)" "IN_PROGRESS(p)" "|" "DONE(d)"))))
 
 (use-package markdown-mode
   :commands (markdown-mode gfm-mode)
