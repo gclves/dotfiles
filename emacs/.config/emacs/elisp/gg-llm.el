9a14d0d5fd34819b17369b55943278a4f532b3ac
Gui Goncalves <guilherme.goncalves@ibusinessfunding.com>
2026-02-09 16:59:02 -0300

    emacs: replace chatgpt-shell with gptel

--- emacs/.config/emacs/elisp/gg-llm.el
+++ emacs/.config/emacs/elisp/gg-llm.el
@@ -1,15 +1,8 @@
-;; use emacs itself for GnuPG pw entry
-;; TODO: move this elsewhere
-(setq epa-pinentry-mode 'loopback
-      epg-pinentry-mode 'loopback)
-
-(use-package chatgpt-shell
-  :bind (("C-x l s" . chatgpt-shell)
-         ("C-x l e" . chatgpt-shell-prompt-compose))
-  :custom
-  ((chatgpt-shell-anthropic-key
-    (lambda ()
-      (auth-source-pass-get 'secret "anthropic-key")))))
+(use-package gptel
+  :config
+  (setq gptel-model 'claude-4.5-sonnet
+        gptel-backend (gptel-make-gh-copilot "Copilot")
+        gptel-default-mode 'org-mode))
 
 
 (provide 'gg-llm)
