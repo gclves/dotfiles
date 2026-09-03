c152121f84d5f958b9dcc35273ffc467736d0e76
Gui Goncalves <guilherme.goncalves@ibusinessfunding.com>
2026-09-03 17:45:01 -0300

    Add trimmed-down newcomers.el

---
+++ emacs/.config/emacs/user-lisp/vendor/newcomers.el
@@ -0,0 +1,28 @@
+(setopt compilation-scroll-output 'first-error
+        completion-styles '(basic emacs22 flex)
+        completions-detailed t
+        completions-group t
+        delete-selection-mode t
+        dired-auto-revert-buffer t
+        font-use-system-font t
+        frame-resize-pixelwise t
+        global-completion-preview-mode t
+        global-xref-mouse-mode t
+        imenu-auto-rescan t
+        minibuffer-visible-completions t
+        mode-line-compact 'long
+        mouse-drag-and-drop-region t
+        mouse-drag-and-drop-region-cross-program t
+        mouse-yank-at-point t
+        package-autosuggest-mode t
+        save-interprogram-paste-before-kill t
+        save-place-mode t
+        savehist-mode t
+        shell-command-prompt-show-cwd t
+        tab-always-indent 'complete
+        view-read-only t
+        window-resize-pixelwise t)
+
+(setq pixel-scroll-mode t)
+
+(provide 'newcomers)
